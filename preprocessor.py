584cd4fda608cf60be5cfeacff6d59742b112632
mehakgupta921 <mehakgupta00921@gmail.com>
2023-04-19 23:11:16 +0530

    Initial Commit

---
+++ preprocessor.py
@@ -0,0 +1,10 @@
+import pandas as pd
+# df=pd.read_csv('events.csv')
+# region_df=pd.read_csv('noc_regions.csv')
+def preprocess(df,region_df):
+
+    df=df[df['Season']=='Summer']
+    df=df.merge(region_df,on='NOC',how='left')
+    df.drop_duplicates(inplace=True)
+    df=pd.concat([df,pd.get_dummies(df['Medal'])],axis=1)
+    return df
